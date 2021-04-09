041044ed20681a273c8879507986c2c2c340257b
fernandams <nanda.sousa.m@gmail.com>
2021-04-08 22:09:51 -0300

    Added verification of user role and changed block content accordingly

--- block_introcomputerscience.php
+++ block_introcomputerscience.php
@@ -14,31 +14,53 @@ class block_introcomputerscience extends block_base {
         
         if (!empty($this->config->selected_quiz)) {
             $selected_quiz = $this->config->selected_quiz;
+
+            $user_id = $USER->id;
+            $user_name = $USER->username;
+            $course_id = $COURSE->id;
+    
+            $grading_info = grade_get_grades($course_id, 'mod', 'quiz', $selected_quiz, $user_id);
+     
+            $user_grade = $grading_info->items[0]->grades[$user_id]->grade;
+            
+            return $user_grade;   
+        } else {
+            return null;
         }
-
-        $user_id = $USER->id;
-        $user_name = $USER->username;
-        $course_id = $COURSE->id;
-
-        $grading_info = grade_get_grades($course_id, 'mod', 'quiz', $selected_quiz, $user_id);
- 
-        $user_grade = $grading_info->items[0]->grades[$user_id]->grade;
-       
-        return $user_grade;
     }
    
     public function get_quiz_module_id() {
         global $DB, $COURSE;
-        $selected_quiz = $this->config->selected_quiz;
+        
+        if (!empty($this->config->selected_quiz)) {
+            $selected_quiz = $this->config->selected_quiz;
+            $sql_query = 'select id from mdl_course_modules where course = ' . $COURSE->id . ' and module = 16 and instance = ' . $selected_quiz . ';';
+            $query_result = $DB->get_record_sql($sql_query);   
+           
+            return $query_result->id;
+        } else {
+            return null;
+        }
+    }
 
-        $sql_query = 'select id from mdl_course_modules where course = ' . $COURSE->id . ' and module = 16 and instance = ' . $selected_quiz . ';';
-        $query_result = $DB->get_record_sql($sql_query);   
-
-        return $query_result->id;
+    public function get_quiz_name() {
+        global $DB;
+        
+        if (!empty($this->config->selected_quiz)) {
+            $selected_quiz = $this->config->selected_quiz;
+            $sql_query = 'select name from mdl_quiz where id = ' . $selected_quiz . ';';
+            $query_result = $DB->get_record_sql($sql_query);   
+           
+            return $query_result->name;
+        } else {
+            return null;
+        }
     }
 
     public function define_text_content() {
-        global $CFG;
+        global $CFG, $COURSE, $USER;
+
+        $context = context_course::instance($COURSE->id, MUST_EXIST);
 
         $presentation_text = '<p class="ics-light-text">Olá! Sou seu Assistente nesta disciplina. Meu objetivo é te auxiliar no seu processo de aprendizado!</p>';
         $hr = '<hr/>';
@@ -47,12 +69,38 @@ class block_introcomputerscience extends block_base {
         $secondary_text = '';
         $quiz_link = $CFG->wwwroot . '/mod/quiz/view.php?id=' . $this->get_quiz_module_id();
 
-        if (empty($this->config->selected_quiz)) {
-            $main_text = '<p>Professor, por favor, selecione o questionário de percepção nas configurações deste bloco!</p>';
+        if (has_capability('moodle/course:update', $context, $USER->id)) {
+            if (empty($this->config->selected_quiz)) {
+                $presentation_text = '<p class="ics-light-text">Olá, professor! Para que o Assistente funcione corretamente, preciso que você me configure. </p>';
+                $main_text = '<ol class="ics-config-list">
+                <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+
+                <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
+                
+                <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
+                </ol>
+                
+                <p>Ainda não há nenhum Formulário de Percepção selecionado.</p>';
+                $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
+            } else {
+                $presentation_text = '<p class="ics-light-text">Olá, professor! Caso deseje alterar as configurações do Assistente, siga os passos abaixo.</p>';
+                $main_text = '<ol dlass="ics-config-list">
+                <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+
+                <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
+                
+                <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
+                </ol>
+                <p><span class="ics-light-text">O questionário selecionado é:</span> ' . $this->get_quiz_name() . '.</p>';
+
+                $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
+            }
         } else {
             $user_grade = $this->get_user_grade();
 
-            if (empty($user_grade)) {
+            if (empty($this->config->selected_quiz)) { 
+                return '';
+            } elseif (empty($user_grade)) {
                 $main_text = '<p>Para que possamos trabalhar bem juntos, preciso que você responda o <a href="' . $quiz_link . '">Formulário de Percepção</a>.</p>';
 
                 $secondary_text = '<p class="ics-light-text">Em caso de dúvidas, procure seu professor, tutor ou monitor.</p>';
--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = 'block_introcomputerscience';  
-$plugin->version = 2021040706;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2021040813;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires  = 2019111800; // YYYYMMDDHH (This is the release version for Moodle 3.8.5)
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = 'v0.1';
