1ac51eba95212758c0593e001a3b91e1cdca3f14
fernandams <nanda.sousa.m@gmail.com>
2021-04-21 16:05:21 -0300

    Prepared config page for test quizes

--- edit_form.php
+++ edit_form.php
@@ -15,8 +15,24 @@ class block_introcomputerscience_edit_form extends block_edit_form {
         foreach ($query_result as $record) {
            $options[$record->id] = $record->name;
         }
-        $select = $mform->addElement('select', 'config_selected_quiz', get_string('config_quiz', 'block_introcomputerscience'), $options);
-        $select->setSelected('3');
+        $mform->addElement('select', 'config_selected_quiz', get_string('config_quiz', 'block_introcomputerscience'), $options);
+        
+        $mform->addElement('html', '<hr/>');
+
+        $mform->addElement('html', '<p>' . get_string('config_instruction', 'block_introcomputerscience') . '</p> <br/>');
+
+        $mform->addElement('select', 'config_list_1', get_string('config_list_1', 'block_introcomputerscience'), $options);
          
+        $mform->addElement('select', 'config_list_2', get_string('config_list_2', 'block_introcomputerscience'), $options);
+      
+        $mform->addElement('select', 'config_list_3', get_string('config_list_3', 'block_introcomputerscience'), $options);
+        
+        $mform->addElement('select', 'config_list_4', get_string('config_list_4', 'block_introcomputerscience'), $options);
+         
+        $mform->addElement('select', 'config_list_5', get_string('config_list_5', 'block_introcomputerscience'), $options);
+
+        $mform->addElement('select', 'config_list_6', get_string('config_list_6', 'block_introcomputerscience'), $options);
+         
+        $mform->addElement('select', 'config_list_7', get_string('config_list_7', 'block_introcomputerscience'), $options);
     }
 }
--- lang/pt_br/block_introcomputerscience.php
+++ lang/pt_br/block_introcomputerscience.php
@@ -6,3 +6,13 @@ $string['introcomputerscience:myaddinstance'] = 'Adicione um novo bloco Assisten
 
 // Config
 $string['config_quiz'] = 'Selecione o questionário de percepção';
+
+$string['config_instruction'] = 'Para cada um dos casos abaixo, selecione o respectivo questionário:';
+
+$string['config_list_1'] = 'Lista 1 - Variáveis e Expressões';
+$string['config_list_2'] = 'Lista 2 - Estrutura de Decisão';
+$string['config_list_3'] = 'Lista 3 - Estrutura de Repetição';
+$string['config_list_4'] = 'Lista 4 - Funções';
+$string['config_list_5'] = 'Lista 5 - Strings';
+$string['config_list_6'] = 'Lista 6 - Listas';
+$string['config_list_7'] = 'Lista 7 - Tuplas e Dicionários';
