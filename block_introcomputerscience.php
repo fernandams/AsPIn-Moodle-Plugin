3e90845dea97dc01e6bd64b558e148e38203d166
fernandams <nanda.sousa.m@gmail.com>
2021-04-22 21:56:54 -0300

    Adapted get_content() to teacher, student and timestamp

--- block_introcomputerscience.php
+++ block_introcomputerscience.php
@@ -57,10 +57,44 @@ class block_introcomputerscience extends block_base {
         }
     }
 
-    public function define_text_content() {
-        global $CFG, $COURSE, $USER;
+    public function define_teacher_text() {
+        global $CFG;
 
-        $context = context_course::instance($COURSE->id, MUST_EXIST);
+        $presentation_text = '';
+        $hr = '<hr/>';
+        $main_text = '';
+        $secondary_text = '';
+
+        if (empty($this->config->selected_quiz)) {
+            $presentation_text = '<p class="ics-light-text">Olá, professor! Para que o Assistente funcione corretamente, preciso que você me configure. </p>';
+            $main_text = '<ol class="ics-config-list">
+            <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+
+            <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
+            
+            <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
+            </ol>
+            
+            <p>Ainda não há nenhum Formulário de Percepção selecionado.</p>';
+            $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
+        } else {
+            $presentation_text = '<p class="ics-light-text">Olá, professor! Caso deseje alterar as configurações do Assistente, siga os passos abaixo.</p>';
+            $main_text = '<ol dlass="ics-config-list">
+            <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+
+            <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
+            
+            <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
+            </ol>
+            <p><span class="ics-light-text">O questionário selecionado é:</span> ' . $this->get_quiz_name() . '.</p>';
+
+            $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
+        }
+        return $presentation_text . $hr . $main_text . $secondary_text;
+    }
+
+    public function define_initial_text() {
+        global $CFG;
 
         $presentation_text = '<p class="ics-light-text">Olá! Sou seu Assistente nesta disciplina. Meu objetivo é te auxiliar no seu processo de aprendizado!</p>';
         $hr = '<hr/>';
@@ -69,72 +103,64 @@ class block_introcomputerscience extends block_base {
         $secondary_text = '';
         $quiz_link = $CFG->wwwroot . '/mod/quiz/view.php?id=' . $this->get_quiz_module_id();
 
-        if (has_capability('moodle/course:update', $context, $USER->id)) {
-            if (empty($this->config->selected_quiz)) {
-                $presentation_text = '<p class="ics-light-text">Olá, professor! Para que o Assistente funcione corretamente, preciso que você me configure. </p>';
-                $main_text = '<ol class="ics-config-list">
-                <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+        $user_grade = $this->get_user_grade();
 
-                <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
-                
-                <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
-                </ol>
-                
-                <p>Ainda não há nenhum Formulário de Percepção selecionado.</p>';
-                $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
-            } else {
-                $presentation_text = '<p class="ics-light-text">Olá, professor! Caso deseje alterar as configurações do Assistente, siga os passos abaixo.</p>';
-                $main_text = '<ol dlass="ics-config-list">
-                <li>Clique no ícone de engrenagem da disciplina e selecione “Ativar edição”;</li>
+        if (empty($this->config->selected_quiz)) { 
+            return '';
+        } elseif (empty($user_grade)) {
+            $main_text = '<p>Para que possamos trabalhar bem juntos, preciso que você responda o <a href="' . $quiz_link . '">Formulário de Percepção</a>.</p>';
 
-                <li>Com a edição ativada, clique na engrenagem deste bloco e selecione “Configurar bloco Assistente de ICC”;</li>
-                
-                <li>Na página de configuração, preencha a seção “Configurações do Bloco” de acordo com suas preferências.</li>
-                </ol>
-                <p><span class="ics-light-text">O questionário selecionado é:</span> ' . $this->get_quiz_name() . '.</p>';
+            $secondary_text = '<p class="ics-light-text">Em caso de dúvidas, procure seu professor, tutor ou monitor.</p>';
+        } elseif ($user_grade == 4) {
+            $main_text = '<p>Não tenho nenhuma recomendação para você no momento, bons estudos!</p>';
 
-                $secondary_text = '<p class="ics-light-text">Uma vez configurado, o Assistente estará pronto para auxiliar seus alunos!</p>';
-            }
-        } else {
-            $user_grade = $this->get_user_grade();
+            $secondary_text = '<p class="ics-light-text">Em caso de dúvidas, procure seu professor, tutor ou monitor.</p>';
+        } elseif (( $user_grade == 2) || ($user_grade == 3)) {
+            $main_text = '<p>Antes de iniciar seus estudos, sugiro que você dedique um pouco de tempo nos conteúdos abaixo:</p>';
 
-            if (empty($this->config->selected_quiz)) { 
-                return '';
-            } elseif (empty($user_grade)) {
-                $main_text = '<p>Para que possamos trabalhar bem juntos, preciso que você responda o <a href="' . $quiz_link . '">Formulário de Percepção</a>.</p>';
+            $recommended_list = '<ul class="ics-list"><li><a href="https://youtube.com">Vídeo sobre Programação em Python;</a></li><li><a href="https://youtube.com">Mais exemplos.</a></li></ul>';
 
-                $secondary_text = '<p class="ics-light-text">Em caso de dúvidas, procure seu professor, tutor ou monitor.</p>';
-            } elseif ($user_grade == 4) {
-                $main_text = '<p>Não tenho nenhuma recomendação para você no momento, bons estudos!</p>';
+            $secondary_text = '<p class="ics-light-text">É importante que você dê uma olhada nesses conteúdos. Trabalhar esses conceitos pode acelerar o seu desempenho nas atividades futuras da disciplina.</p>';
+        } elseif (($user_grade == 0) || ($user_grade == 1)) {
+            $main_text = '<p>Antes de iniciar seus estudos, sugiro que você dedique um pouco de tempo nos conteúdos abaixo:</p>';
 
-                $secondary_text = '<p class="ics-light-text">Em caso de dúvidas, procure seu professor, tutor ou monitor.</p>';
-            } elseif (( $user_grade == 2) || ($user_grade == 3)) {
-                $main_text = '<p>Antes de iniciar seus estudos, sugiro que você dedique um pouco de tempo nos conteúdos abaixo:</p>';
+            $recommended_list = '<ul class="ics-list"><li><a href="https://youtube.com">Introdução ao Pensamento Computacional;</a></li><li><a href="https://youtube.com">Vídeo sobre Lógica de Programação;</a></li><li><a href="https://youtube.com">Mais exemplos.</a></li></ul>';
 
-                $recommended_list = '<ul class="ics-list"><li><a href="https://youtube.com">Vídeo sobre Programação em Python;</a></li><li><a href="https://youtube.com">Mais exemplos.</a></li></ul>';
-
-                $secondary_text = '<p class="ics-light-text">É importante que você dê uma olhada nesses conteúdos. Trabalhar esses conceitos pode acelerar o seu desempenho nas atividades futuras da disciplina.</p>';
-            } elseif (($user_grade == 0) || ($user_grade == 1)) {
-                $main_text = '<p>Antes de iniciar seus estudos, sugiro que você dedique um pouco de tempo nos conteúdos abaixo:</p>';
-
-                $recommended_list = '<ul class="ics-list"><li><a href="https://youtube.com">Introdução ao Pensamento Computacional;</a></li><li><a href="https://youtube.com">Vídeo sobre Lógica de Programação;</a></li><li><a href="https://youtube.com">Mais exemplos.</a></li></ul>';
-
-                $secondary_text = '<p class="ics-light-text">É importante que você dê uma olhada nesses conteúdos. Trabalhar esses conceitos pode acelerar o seu desempenho nas atividades futuras da disciplina.</p>';
-            }
+            $secondary_text = '<p class="ics-light-text">É importante que você dê uma olhada nesses conteúdos. Trabalhar esses conceitos pode acelerar o seu desempenho nas atividades futuras da disciplina.</p>';
         }
 
         return $presentation_text . $hr . $main_text . $recommended_list . $secondary_text;
     }
 
+    public function get_timeclose_quiz($quiz_id) {
+        global $COURSE, $DB;
+
+        $sql_query = 'select timeclose from mdl_quiz where course = ' . $COURSE->id . ' and id = ' . $quiz_id . ';';
+        $query_result = $DB->get_record_sql($sql_query);
+        return $query_result->timeclose;
+    }
+
     public function get_content() {
+        global $CFG, $USER, $COURSE;
+
+        $context = context_course::instance($COURSE->id, MUST_EXIST);
+
         if ($this->content !== null) {
             return $this->content;
         }
      
         $this->content =  new stdClass;
-        $this->content->text = $this->define_text_content();
+
+        if (has_capability('moodle/course:update', $context, $USER->id)) {
+            $this->content->text = $this->define_teacher_text();
+        } elseif (!empty($this->config->list_1) && ($this->get_timeclose_quiz($this->config->list_1) != 0) && (time() > $this->get_timeclose_quiz($this->config->list_1))) {
+            $this->content->text = time();
+        } else {
+            $this->content->text = $this->define_initial_text();
+        }
         
         return $this->content;
 
     }
 }
+    
