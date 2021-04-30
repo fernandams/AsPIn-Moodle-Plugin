32470e1f0d7b0cb80947b71e9045c6140148d902
fernandams <nanda.sousa.m@gmail.com>
2021-04-29 22:22:05 -0300

    Recommendation for lists function done

--- block_introcomputerscience.php
+++ block_introcomputerscience.php
@@ -84,21 +84,101 @@ class block_introcomputerscience extends block_base {
     }
 
     public function define_list_support_text() {
-        if (!empty($this->config->list_7) && (get_timeclose_quiz($this->config->list_7) != 0) && (time() > get_timeclose_quiz($this->config->list_7))) {
-            return "Quiz 7";
-        } elseif (!empty($this->config->list_6) && (get_timeclose_quiz($this->config->list_6) != 0) && (time() > get_timeclose_quiz($this->config->list_6))) {
-            return "Quiz 6";
-        } elseif (!empty($this->config->list_5) && (get_timeclose_quiz($this->config->list_5) != 0) && (time() > get_timeclose_quiz($this->config->list_5))) {
-            return "Quiz 5";
-        } elseif (!empty($this->config->list_4) && (get_timeclose_quiz($this->config->list_4) != 0) && (time() > get_timeclose_quiz($this->config->list_4))) {
-            return "Quiz 4";
-        } elseif (!empty($this->config->list_3) && (get_timeclose_quiz($this->config->list_3) != 0) && (time() > get_timeclose_quiz($this->config->list_3))) {
-            return "Quiz 3";
-        } elseif (!empty($this->config->list_2) && (get_timeclose_quiz($this->config->list_2) != 0) && (time() > get_timeclose_quiz($this->config->list_2))) {
-            return "Quiz 2";
+        $presentation_text = '<p class="ics-light-text">Olá! Sou seu Assistente nesta disciplina. Meu objetivo é te auxiliar no seu processo de aprendizado!</p>';
+        $hr = '<hr/>';
+        $main_text = '';
+        $recommended_array = [];
+        $subjects = [];
+        $subjects_text = '';
+
+        $list_1 = $this->config->list_1;
+        $list_2 = $this->config->list_2;
+        $list_3 = $this->config->list_3;
+        $list_4 = $this->config->list_4;
+        $list_5 = $this->config->list_5;
+        $list_6 = $this->config->list_6;
+        $list_7 = $this->config->list_7;
+
+        
+        if (get_user_grade($list_1) > 5) {
+            $main_text = '<p>Parabéns pelo seu desempenho na lista 1! Continue assim!';
         } else {
-            return "Quiz 1 Nota: " . get_user_grade($this->config->list_1);
+            $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+            array_push($subjects, 'Variáveis e Expressões');
+            array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Variáveis e Expressões.</a></li></ul>');
         }
+
+        if (!empty($list_2) && (get_timeclose_quiz($list_2) != 0) && (time() > get_timeclose_quiz($list_2))) {
+            if (get_user_grade($list_2) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 2! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Estrutura de Decisão');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Estrutura de Decisão.</a></li></ul>');
+            }
+        }         
+        
+        if (!empty($list_3) && (get_timeclose_quiz($list_3) != 0) && (time() > get_timeclose_quiz($list_3))) {
+            if (get_user_grade($list_3) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 3! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Estrutura de Repetição');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Estrutura de Repetição.</a></li></ul>');
+            }
+        } 
+
+        if (!empty($list_4) && (get_timeclose_quiz($list_4) != 0) && (time() > get_timeclose_quiz($list_4))) {
+            if (get_user_grade($list_4) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 4! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Funções');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Funções.</a></li></ul>');
+            }
+        } 
+        
+        if (!empty($list_5) && (get_timeclose_quiz($list_5) != 0) && (time() > get_timeclose_quiz($list_5))) {
+            if (get_user_grade($list_5) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 5! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Strings');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Strings.</a></li></ul>');
+            }
+        } 
+        
+        if (!empty($list_6) && (get_timeclose_quiz($list_6) != 0) && (time() > get_timeclose_quiz($list_6))) {
+            if (get_user_grade($list_6) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 6! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Listas');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Listas.</a></li></ul>');
+            }
+        } 
+
+        if (!empty($list_7) && (get_timeclose_quiz($list_7) != 0) && (time() > get_timeclose_quiz($list_7))) {
+            if (get_user_grade($list_7) > 5) {
+                $main_text = '<p>Parabéns pelo seu desempenho na lista 7! Continue assim!';
+            } else {
+                $main_text = '<p>Parabéns pela sua dedicação em realizar as listas! Continue se esforçando para aprender o conteúdo!';
+                array_push($subjects, 'Tuplas e Dicionários');
+                array_push($recommended_array, '<ul class="ics-list"><li><a href="https://youtube.com">Exercícios contextualizados de Tuplas e Dicionários.</a></li></ul>');
+            }
+        } 
+
+
+        if (sizeof($subjects) > 0) {
+            $main_text = $main_text . '<br/>Notei que você pode ter tido dificuldade em alguns conteúdos.</p>';
+            foreach ($subjects as $key=>$subject) {
+               $subjects_text = $subjects_text . '<p class="ics-light-text">Para ' . $subject . ', dê uma olhada em:</p>' . $recommended_array[$key];
+            }
+        } else {
+            $main_text = $main_text . '</p>';
+        }
+
+        return $presentation_text . $hr . $main_text . $subjects_text;
     }
 
     public function get_content() {
