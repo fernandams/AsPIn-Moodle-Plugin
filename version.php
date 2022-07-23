4fd97ab973c11cbced8ad1d5d7baaf47e11be784
fernandams <nanda.sousa.m@gmail.com>
2022-07-23 18:41:42 -0300

    Updated Moodle version

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = 'block_introcomputerscience';  
-$plugin->version = 2021082501;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires  = 2020110900; // YYYYMMDDHH (This is the release version for Moodle 3.10)
+$plugin->version = 2022072300;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires  = 2022041900; // YYYYMMDDHH (This is the release version for Moodle 4.0)
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = 'v1.0';
